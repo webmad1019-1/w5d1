dea227973e5cf9e96c7fe702e9ab80c830e6799b
Dani Vicario <danivicario@dvicario.es>
2019-11-18 01:29:55 +0100

    added

--- slack-login/passport/serializers.js
+++ slack-login/passport/serializers.js
@@ -5,11 +5,11 @@ passport.serializeUser((user, cb) => {
   cb(null, user._id);
 });
 
-passport.deserializeUser((id, cbo) => {
+passport.deserializeUser((id, cb) => {
   User.findById(id, (err, user) => {
     if (err) {
       return cb(err);
     }
-    cbo(null, user);
+    cb(null, user);
   });
 });
